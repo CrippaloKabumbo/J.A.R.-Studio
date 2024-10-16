f052885c3833eb06d0889e67648a00594a55592e
CrippaloKabumbo <abed6861@mylaurier.ca>
2024-10-16 04:22:32 -0400

    test commit 2

--- Assets/NPCInteraction.cs
+++ Assets/NPCInteraction.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class NPCInteraction : MonoBehaviour
 {
+    // setting the interact key
+    public KeyCode interactionKey = KeyCode.Space;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check interaction key press
+        if (Input.GetKeyDown(interactionKey))
+        {
+            // check player interaction
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.CompareTag("Player"))
+            {
+                Interact();
+            }
+        }
+
+    }
+    void Interact()
+    {
+        // Logic for interacting with the NPC
+        Debug.Log("Interacted with NPC: " + gameObject.name);
     }
 }
